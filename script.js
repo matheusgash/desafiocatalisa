73598cfe9ea6fa54b495f77ec66996306977cf20
Matheus <matheusxDD1@hotmail.com>
2022-01-15 16:31:28 -0300

    implementar metodo GET ao JS

--- script.js
+++ script.js
@@ -6,5 +6,24 @@ const statusPersonagem = document.querySelector('#status');
 
 gerarValorAleatorio = () => {
     const numeros = [5, 50, 500];
-    return Math.floor(Math.random() * numeros.length);
+    const index = Math.floor(Math.random() * numeros.length);
+    const numGerado = numeros[index];
+    return numGerado;
 }
+
+getPersonagem = () => {
+    return fetch(`https://rickandmortyapi.com/api/character/${gerarValorAleatorio()}`, {
+        method:'GET', 
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        img.src = data.image;
+        img.alt = data.name;
+        nomePersonagem.innerHTML = data.name;
+        especiePersonagem.innerHTML = data.species;
+        statusPersonagem.innerHTML = data.status;
+    });
+}
+botao.onclick = getPersonagem;
